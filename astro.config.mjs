b18b38655037d6a8f24ef3e3343ab1db07fb6680
fabianbarua <baruafabian@gmail.com>
2024-10-07 14:16:26 -0300

    add SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 export default defineConfig({
+  site: "https://interbrasoficial.com/",
   i18n: {
     defaultLocale: "es",
     locales: ["es", "pt"],
